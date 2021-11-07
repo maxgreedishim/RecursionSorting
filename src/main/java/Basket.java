57aa482fef253d2fe9152f013045830d96cda3d5
maxgreedishim <90276401+maxgreedishim@users.noreply.github.com>
2021-11-07 17:05:25 +0500

    сортировка рекурсией

---
+++ src/main/java/Basket.java
@@ -0,0 +1,6 @@
+public class Basket {
+     /*for(int i = from + 1; i <= middle; i++) {
+            min = Math.min(array[i], min);
+            System.out.println("Значения array[i]: " + array[i]);
+        }*/
+}
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,4 +1,5 @@
 import java.io.UnsupportedEncodingException;
+import java.util.Arrays;
 import java.util.PrimitiveIterator;
 import java.util.Random;
 import java.util.Scanner;
@@ -16,69 +17,94 @@ public class Main {
             new Random().ints(MIN_VALUE,MAX_VALUE ).iterator();
 
 
-    /*public static void main(String[] args) {
-       int[] array = new int[ARRAY_LENGTH];
-       System.out.print("исходный массив: ");
-       for (int i = 0; i < array.length; i++) {
-           array[i] = randomIntsGenerator.nextInt();
-           System.out.print(array[i] + " ");
-       }
-        System.out.println();
-
-    }*/
     public static void main(String[] args) {
         int[] array = new int[ARRAY_LENGTH];
-        for(int i = 0; i < array.length; i++) array[i] = randomIntsGenerator.nextInt();
+        for(int i = 0; i < array.length; i++)
+            array[i] = randomIntsGenerator.nextInt();
         printArray(array, "     исходное состояние массива");
+        quickSort(array, 0, array.length -1);
+        System.out.println("Стало");
+        System.out.println(Arrays.toString(array));
+        //printArray(array, "половина массива ");
+        //sort(array, 0, array.length - 1 );
+        //printArray(array, "         отсортированный массив");
 
-        sort(array, 0, array.length - 1);
-        printArray(array, "         отсортированный массив");
     }
-    static int rightHasLess(int[] array, int from, int middle, int to){
-        int min = array[from];
-        for(int i = from + 1; i <= to; i++) {
-            if(array[i] >= min) continue;
-            if(i > middle) return i;
-            min = array[i];
+    public static void quickSort(int[] array, int from, int to) {
+        int middle = from + (to - from) / 2;
+        int opora = array[middle];
+        // разделить на подмассивы, который больше и меньше опорного элемента
+        while (from <= to) {
+            while (array[from] < opora) {
+                from++;
+            }
+
+            while (array[to] > opora) {
+                to--;
+            }
+
+            if (from <= to) {//меняем местами
+                int temp = array[from];
+                array[from] = array[to];
+                array[to] = temp;
+                from++;
+                to--;
+            }
         }
-        return -1;
+        // вызов рекурсии для сортировки левой и правой части
+
+        System.out.println("маркер:" + middle);
+        sort(array, from, middle);
+        sort(array, middle + 1, to);
+        merge(array, from, middle, to);
     }
-    static void swap(int[] array, int from, int middle, int to, boolean recursive){
+
+
+    public static void swap(int[] array, int from, int middle, int to, boolean recursive){
+
         int min = array[from];
-        for(int i = from + 1; i <= middle; i++) {
-            min = Math.min(array[i], min);
-        }
+        System.out.println("Значения min:" + min);
+
         int indexOfLess = -1;
         for(int i = middle + 1; i <= to; i++){
             if(array[i] >= min) continue;
             indexOfLess = i;
+            System.out.println("индекс i: " + i);
             break;
         }
         if(indexOfLess == -1) return;
         int tmp = array[indexOfLess];
         array[indexOfLess] = array[middle];
         array[middle] = tmp;
+        System.out.println("tmp: " + tmp);
         if(recursive) sort(array, from, to);
         printArray(array, "промежуточное состояние массива");
+
     }
     public static void sort(int[] array, int from, int to){
 
         if(!(from < to)) return;
-        int middle = getMiddle(from, to);
+        //int middle = getMiddle(from, to);
+        int middle = from;
+        System.out.println("маркер:" + middle);
         sort(array, from, middle);
         sort(array, middle + 1, to);
         merge(array, from, middle, to);
+
     }
 
     static void merge(int[] array, int from, int middle, int to){
         swapRecursive(array, from, middle, to);
+
     }
     static void printArray(int[] array, String description){
         System.out.print(description + ": ");
         for (int i : array) System.out.print(i + " ");
         System.out.println();
+
     }
     static void swapRecursive(int[] array, int from, int middle, int to){
+
         swap(array, from, middle, to, true);
     }
 
@@ -86,9 +112,8 @@ public class Main {
         if (from < MIN_BOUND_VALUE) throw new ArithmeticException("Меньший индекс не может быть меньше 0");
         if (to > MAX_BOUND_VALUE) throw new ArithmeticException("Недопустить перерасход памяти");
         if (!(from < to)) throw new ArithmeticException("Начальный индекс должен быть меньше конечного");
-        double length = to + from;
-        double middle = Math.floor(length / 2);
-        return (int)middle;
+        double middle = from;
+        return (int) middle;
     }
 
    //Напишите числа a и b, сумма которых будет равна 4
